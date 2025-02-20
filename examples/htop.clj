25073398d84cd3f9c0b2a5000984cc7c377b41f9
rocketorbit <1284604827@qq.com>
2025-02-21 01:23:06 +0800

    modified:   examples/htop.clj

--- examples/htop.clj
+++ examples/htop.clj
@@ -10,8 +10,6 @@
             (def cpu (+ cpu (val (slice (at task 2) 0 (- 0 1)))))
             (def mem (+ mem (val (slice (at task 3) 0 (- 0 1)))))
             (push tasks (if (== (at task 0) 'root') (join (list '<color=#ff4b4b>' (at task 0) '</color> <color=#20ff98>' (at task 1) '</color> <color=#21bcff>' (at task 2) '</color> <color=#21bcff>' (at task 3) '</color> <color=#baff50>' (at task 4) '</color>') '') (join (list '<color=#fbfbfb>' (at task 0) '</color> <color=#20ff98>' (at task 1) '</color> <color=#21bcff>' (at task 2) '</color> <color=#21bcff>' (at task 3) '</color> <color=#baff50>' (at task 4) '</color>') '')))))
-        (wait (/ (! (print (join (list '<color=#fbfbfb>tasks: ' (len tasks) '</color>\\n<color=#fbfbfb>cpu_usage: [</color>' (bar cpu 25) '<color=#fbfbfb>]==[ </color><color=#21bcff>' cpu '%</color> <color=#fbfbfb>]</color>\n<color=#fbfbfb>mem_usage: [</color>' (bar mem 25) '<color=#fbfbfb>]==[ </color><color=#21bcff>' mem '%</color> <color=#fbfbfb>]</color>\\n' (format_columns (join (+ (list '<color=#9d9d9d>USER</color> <color=#9d9d9d>PID</color> <color=#9d9d9d>CPU</color> <color=#9d9d9d>MEM</color> <color=#9d9d9d>COLOR</color>') tasks) '\\n'))) '') 1)) 2))
-    ))
-)))
+        (wait (/ (! (print (join (list '<color=#fbfbfb>tasks: ' (len tasks) '</color>\\n<color=#fbfbfb>cpu_usage: [</color>' (bar cpu 25) '<color=#fbfbfb>]==[ </color><color=#21bcff>' cpu '%</color> <color=#fbfbfb>]</color>\n<color=#fbfbfb>mem_usage: [</color>' (bar mem 25) '<color=#fbfbfb>]==[ </color><color=#21bcff>' mem '%</color> <color=#fbfbfb>]</color>\\n' (format_columns (join (+ (list '<color=#9d9d9d>USER</color> <color=#9d9d9d>PID</color> <color=#9d9d9d>CPU</color> <color=#9d9d9d>MEM</color> <color=#9d9d9d>COLOR</color>') tasks) '\\n'))) '') 1)) 2)))))))
 
 (def htop-with-stop (lambda () ('TODO'))) ;TODO
