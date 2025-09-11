07a873b72378b9a33f73b8d9b53b2cdc6c9ab307
rocketorbit <1284604827@qq.com>
2025-09-12 02:17:51 +0800

    modified:   examples/forLoop.clj

--- examples/forLoop.clj
+++ examples/forLoop.clj
@@ -1,16 +1,16 @@
 ;; Macro that implements a C-like for loop
-(defmacro for (initializer condition iterator body) () ((lambda ()
+(defmacro my-for (initializer condition iterator body) () ((lambda ()
     initializer
     (while condition (begin
         body
         iterator)))))
 
 ;; Usage example
-(for (def i 0) (< i 5) (def i (+ i 1)) (begin ;; begin is unneccessary if loop evaluates one expression only
+(my-for (def i 0) (< i 5) (def i (+ i 1)) (begin ;; begin is unneccessary if loop evaluates one expression only
     (print i)))
 
 ;; Macro that implements a foreach loop
-(defmacro foreach (container k v body) () ((lambda ()
+(defmacro my-foreach (container k v body) () ((lambda ()
     (def idxs (indexes container))
     (while (len idxs) (begin
         (def k (pull idxs))
@@ -18,5 +18,5 @@
         body)))))
 
 ;; Usage example
-(foreach 'Hello world!' _ v  (begin ;; begin is unneccessary if loop evaluates one expression only
+(my-foreach 'Hello world!' _ v  (begin ;; begin is unneccessary if loop evaluates one expression only
     (print v)))
